4fda98537b49b58fad13ba1b4a794045c7115aba
Roman Scherer <roman@burningswell.com>
2024-08-25 13:57:47 +0200

    Add asahi/guix/maintenance/channels.scm

---
+++ asahi/guix/maintenance/channels.scm
@@ -0,0 +1,56 @@
+(define-module (asahi guix maintenance channels)
+  #:use-module (guix channels))
+
+(define-public %asahi-channel
+  (channel
+   (name 'asahi-channel)
+   (branch "main")
+   (url "https://github.com/asahi-guix/channel")
+   (introduction
+    (make-channel-introduction
+     "3eeb493b037bea44f225c4314c5556aa25aff36c"
+     (openpgp-fingerprint
+      "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
+
+(define-public %asahi-guix
+  (channel
+   (name 'guix)
+   (url "https://github.com/asahi-guix/guix")
+   (branch "main")
+   (introduction
+    (make-channel-introduction
+     "59c86958338970cac132f45da37de3b00a26a8cc"
+     (openpgp-fingerprint
+      "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
+
+(define-public %asahi-maintenance
+  (channel
+   (name 'asahi-maintenance)
+   (branch "main")
+   (url "https://github.com/asahi-guix/maintenance")
+   (introduction
+    (make-channel-introduction
+     "c665797c5065cbed81b0c4a9e121baff1f2ffcc0"
+     (openpgp-fingerprint
+      "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))
+
+(define-public %gnu-guix
+  (channel
+   (name 'guix)
+   (url "https://git.savannah.gnu.org/git/guix")
+   (introduction
+    (make-channel-introduction
+     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     (openpgp-fingerprint
+      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+
+(define-public %gnu-guix-core-updates
+  (channel
+   (name 'guix)
+   (url "https://git.savannah.gnu.org/git/guix")
+   (branch "core-updates")
+   (introduction
+    (make-channel-introduction
+     "9edb3f66fd807b096b48283debdcddccfea34bad"
+     (openpgp-fingerprint
+      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
