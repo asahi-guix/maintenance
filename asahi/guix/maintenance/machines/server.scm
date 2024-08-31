6e172cd72afdea9f58ba42c448c7e4d4ad04f0a0
Roman Scherer <roman@burningswell.com>
2024-08-31 14:25:30 +0200

    Revert "Comment code"
    This reverts commit 221a9a9aa60165001fe5cbc6542030b817d99123.

--- asahi/guix/maintenance/machines/server.scm
+++ asahi/guix/maintenance/machines/server.scm
@@ -2,22 +2,23 @@
   #:use-module (asahi guix maintenance systems server)
   #:use-module (gnu machine ssh)
   #:use-module (gnu machine)
-  #:use-module (gnu))
+  #:use-module (gnu)
+  #:export (asahi-guix-server-machine))
 
-;; (define host-key
-;;   (string-append "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDGYVv+hWFyx/CaKC0li/8mpnvzCD6mnyJKytoIxErX root@(none)"))
+(define host-key
+  (string-append "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDGYVv+hWFyx/CaKC0li/8mpnvzCD6mnyJKytoIxErX root@(none)"))
 
-;; (define-public asahi-guix-server-machine
-;;   (machine
-;;    (operating-system asahi-guix-server-system)
-;;    (environment managed-host-environment-type)
-;;    (configuration (machine-ssh-configuration
-;;                    (allow-downgrades? #t)
-;;                    (build-locally? #f)
-;;                    (host-key host-key)
-;;                    (host-name "www.asahi-guix.org")
-;;                    (port 22)
-;;                    (system "aarch64-linux")
-;;                    (user "root")))))
+(define asahi-guix-server-machine
+  (machine
+   (operating-system asahi-guix-server-system)
+   (environment managed-host-environment-type)
+   (configuration (machine-ssh-configuration
+                   (allow-downgrades? #t)
+                   (build-locally? #f)
+                   (host-key host-key)
+                   (host-name "www.asahi-guix.org")
+                   (port 22)
+                   (system "aarch64-linux")
+                   (user "root")))))
 
-;; (list asahi-guix-server-machine)
+(list asahi-guix-server-machine)
--- asahi/guix/maintenance/manifest.scm
+++ asahi/guix/maintenance/manifest.scm
@@ -6,15 +6,15 @@
   #:use-module (guix packages)
   #:use-module (guix profiles))
 
-;; (define %asahi-guix-version "0.0.1")
+(define %asahi-guix-version "0.0.1")
 
-;; (define %asahi-guix-server
-;;   (manifest-entry
-;;     (name "asahi-guix-server")
-;;     (version %asahi-guix-version)
-;;     (item asahi-guix-server-system)))
+(define %asahi-guix-server
+  (manifest-entry
+    (name "asahi-guix-server")
+    (version %asahi-guix-version)
+    (item asahi-guix-server-system)))
 
-;; ;; (concatenate-manifests
-;; ;;  (list (manifest (list %asahi-guix-server))))
+;; (concatenate-manifests
+;;  (list (manifest (list %asahi-guix-server))))
 
-;; (manifest (list %asahi-guix-server))
+(manifest (list %asahi-guix-server))
