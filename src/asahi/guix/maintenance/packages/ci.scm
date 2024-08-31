6d3cd5b639a320968539b5bf475a7b946204eb72
Roman Scherer <roman@burningswell.com>
2024-08-31 15:28:24 +0200

    Use local-file again

--- src/asahi/guix/maintenance/packages/ci.scm
+++ src/asahi/guix/maintenance/packages/ci.scm
@@ -4,15 +4,10 @@
   #:use-module (guix packages)
   #:use-module (gnu packages))
 
-;; (define %disable-jit-patch
-;;   (local-file "../patches/cuirass-disable-jit.patch"))
-
-;; (define-public cuirass-disable-jit
-;;   (package
-;;     (inherit (package-with-patches ci:cuirass (list %disable-jit-patch)))
-;;     (name "cuirass-disable-jit")))
+(define %disable-jit-patch
+  (local-file "../patches/cuirass-disable-jit.patch"))
 
 (define-public cuirass-disable-jit
   (package
-    (inherit ci:cuirass)
+    (inherit (package-with-patches ci:cuirass (list %disable-jit-patch)))
     (name "cuirass-disable-jit")))
