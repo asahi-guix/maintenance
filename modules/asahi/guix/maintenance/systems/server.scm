4b78a2fe1aed9e198d43e0bc621126135c5dcccf
Roman Scherer <roman@burningswell.com>
2024-09-01 20:47:50 +0200

    Add priority to cuirass specification

--- modules/asahi/guix/maintenance/systems/server.scm
+++ modules/asahi/guix/maintenance/systems/server.scm
@@ -174,25 +174,29 @@ COMMIT
            (build '(channels asahi-guix-channel))
            (channels (list #$(channel->code %asahi-guix-channel)
                            #$(channel->code %gnu-guix)))
-           (systems '("aarch64-linux")))
+           (systems '("aarch64-linux"))
+           (priority 8))
           (specification
            (name "asahi-guix-maintenance")
            (build '(manifests ".guix/manifest.scm"))
            (channels (list #$(channel->code %asahi-guix-maintenance)
                            #$(channel->code %gnu-guix)))
-           (systems '("aarch64-linux")))
-          (specification
-           (name "r0man-channel")
-           (build '(channels r0man-channel))
-           (channels (list #$(channel->code %gnu-guix)
-                           #$(channel->code %r0man-channel)))
-           (systems '("aarch64-linux")))
+           (systems '("aarch64-linux"))
+           (priority 9))
           (specification
            (name "asahi-guix-manifest")
            (build '(manifests ".guix/manifest.scm"))
            (channels (list #$(channel->code %asahi-guix-channel)
                            #$(channel->code %gnu-guix)))
-           (systems '("aarch64-linux")))))
+           (systems '("aarch64-linux"))
+           (priority 7))
+          (specification
+           (name "r0man-channel")
+           (build '(channels r0man-channel))
+           (channels (list #$(channel->code %gnu-guix)
+                           #$(channel->code %r0man-channel)))
+           (systems '("aarch64-linux"))
+           (priority 7))))
 
 (define %cuirass-service
   (service cuirass-service-type
