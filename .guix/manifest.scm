4c54f77ecf59fc841e791f1c94e7bcf78904853a
Roman Scherer <roman@burningswell.com>
2024-09-01 12:15:09 +0200

    Revert "Use server module"
    This reverts commit 706e9ed526cc83931418b4820464d60a371b936e.

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,16 +1,7 @@
 (use-modules (guix)
              (guix profiles)
              (gnu packages base)
-             (asahi-guix-maintenance-package)
-             (asahi guix maintenance systems server))
-
-;; (define %asahi-guix-version "0.1")
-
-;; (define %asahi-guix-base
-;;   (manifest-entry
-;;     (name "asahi-guix-base")
-;;     (version %asahi-guix-version)
-;;     (item asahi-base-os)))
+             (asahi-guix-maintenance-package))
 
 ;; (packages->manifest (list asahi-guix-maintenance))
 
