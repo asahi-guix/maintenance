706e9ed526cc83931418b4820464d60a371b936e
Roman Scherer <roman@burningswell.com>
2024-09-01 10:36:16 +0200

    Use server module

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,7 +1,16 @@
 (use-modules (guix)
              (guix profiles)
              (gnu packages base)
-             (asahi-guix-maintenance-package))
+             (asahi-guix-maintenance-package)
+             (asahi guix maintenance systems server))
+
+;; (define %asahi-guix-version "0.1")
+
+;; (define %asahi-guix-base
+;;   (manifest-entry
+;;     (name "asahi-guix-base")
+;;     (version %asahi-guix-version)
+;;     (item asahi-base-os)))
 
 ;; (packages->manifest (list asahi-guix-maintenance))
 
