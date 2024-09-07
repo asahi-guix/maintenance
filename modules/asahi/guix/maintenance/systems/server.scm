a6f47d74f5b6459ae62acd560a06dbcff530e075
Roman Scherer <roman@burningswell.com>
2024-09-07 14:46:58 +0200

    Disable remote server again

--- modules/asahi/guix/maintenance/systems/server.scm
+++ modules/asahi/guix/maintenance/systems/server.scm
@@ -205,11 +205,12 @@ COMMIT
             (host "localhost")
             (port 8081)
             (specifications %cuirass-specifications)
-            (remote-server
-             (cuirass-remote-server-configuration
-              (backend-port 5555)
-              (log-port 5556)
-              (publish-port 5557))))))
+            ;; (remote-server
+            ;;  (cuirass-remote-server-configuration
+            ;;   (backend-port 5555)
+            ;;   (log-port 5556)
+            ;;   (publish-port 5557)))
+            )))
 
 (define %cuirass-remote-worker-service
   (service cuirass-remote-worker-service-type
