1310e1ab90229d8af0ab81c6eed8829e0def0475
Roman Scherer <roman@burningswell.com>
2024-10-09 20:29:13 +0200

    Add website build module

--- modules/asahi/guix/maintenance/build/website.scm
+++ modules/asahi/guix/maintenance/build/website.scm
@@ -1,5 +1,6 @@
-(define-module (asahi guix maintenance build nightly)
-  #:use-module (asahi guix build installer)
+(define-module (asahi guix maintenance build website)
+  #:use-module (asahi guix build installer metadata)
+  #:use-module (asahi guix build installer os)
   #:use-module (ice-9 string-fun)
   #:use-module (guix base16)
   #:use-module (guix build utils)
@@ -17,16 +18,13 @@
   #:use-module (srfi srfi-1))
 
 (define %build-time-format
-  "%Y-%m-%dT%H:%M:%S")
-
-(define %build-time-format
-  "%Y%m%d%H%M%S")
+  "%Y-%m-%d %H:%M:%S")
 
 (define %installer-os-dir
   "share/asahi-installer/os")
 
 (define %output-dir
-  "/tmp/asahi/guix/nightly")
+  "/tmp/asahi-guix/website")
 
 (define-record-type* <website-builder>
   website-builder
@@ -43,20 +41,20 @@
   website-package?
   (build-time website-package-build-time)
   (derivation website-package-derivation)
-  (installer-data website-package-installer-data)
+  (installer-metadata website-package-installer-metadata)
   (log-file website-package-log-file))
 
 (define (website-builder-metadata-path builder)
   (string-append (website-builder-output-dir builder) "/installer_data.json"))
 
-(define (website-builder-installer-data builder)
+(define (website-builder-installer-metadata builder)
   (let ((packages (website-builder-packages builder)))
-    (reduce merge-installer-data #f (map website-package-installer-data packages))))
+    (reduce merge-installer-metadata #f (map website-package-installer-metadata packages))))
 
 (define (website-package-name package)
-  (let ((data (website-package-installer-data package)))
-    (when (installer-data? data)
-      (let ((os (car (installer-data-os-list data))))
+  (let ((data (website-package-installer-metadata package)))
+    (when (installer-metadata? data)
+      (let ((os (car (installer-metadata-os-list data))))
         (when (installer-os? os)
           (installer-os-package os))))))
 
@@ -66,7 +64,7 @@
       (match:substring matches 1))))
 
 (define (installer-package-derivation-path? filename)
-  (string-match ".*-asahi-guix-(.+)-installer-package-(.+)\\.drv" (basename filename)))
+  (string-match ".*-asahi-installer-package-(.+)\\.drv" (basename filename)))
 
 (define (find-installer-package-derivation-paths store-path)
   (map (lambda (path)
@@ -82,19 +80,30 @@
 (define (derivation-installer-os-dir derivation)
   (string-append (derivation->output-path derivation) "/" %installer-os-dir))
 
+(define (derivation-installer-os-dir-exists? derivation)
+  (directory-exists? (derivation-installer-os-dir derivation)))
+
 (define (derivation-build-time derivation)
   (when (derivation-output-path-exists? derivation)
     (stat:ctime (stat (derivation->output-path derivation)))))
 
-(define (derivation-installer-data-files derivation)
+(define (derivation-installer-metadata-files derivation)
   (when (derivation-output-path-exists? derivation)
     (find-files (derivation-installer-os-dir derivation)
                 (lambda (path stats)
                   (string-suffix? ".json" path)))))
 
-(define (derivation-installer-data derivation)
-  (let ((files (derivation-installer-data-files derivation)))
-    (reduce merge-installer-data #f (map read-installer-data files))))
+(define (derivation-installer-metadata derivation)
+  (let ((files (derivation-installer-metadata-files derivation)))
+    (reduce merge-installer-metadata #f
+            (remove null? (map (lambda (file)
+                                 (with-exception-handler
+                                     (lambda (e)
+                                       (format #t "Warning: Invalid installer metadata ~a.\n" file)
+                                       #f)
+                                   (lambda () (read-installer-metadata file))
+                                   #:unwind? #t))
+                               files)))))
 
 (define (compare-build-time package-1 package-2)
   (> (website-package-build-time package-1)
@@ -106,9 +115,9 @@
                  (website-package
                   (build-time (derivation-build-time derivation))
                   (derivation derivation)
-                  (installer-data (derivation-installer-data derivation))
+                  (installer-metadata (derivation-installer-metadata derivation))
                   (log-file (log-file store (derivation-file-name derivation)))))
-               (filter derivation-output-path-exists?
+               (filter derivation-installer-os-dir-exists?
                        (find-installer-package-derivations store-path)))
           compare-build-time)))
 
@@ -152,37 +161,41 @@
 ;; (begin (deploy-website my-builder) #f)
 
 (define (deploy-package-installer-metadata builder package)
-  (let ((data (website-package-installer-data package)))
-    (installer-data
+  (let ((data (website-package-installer-metadata package)))
+    (installer-metadata
      (inherit data)
      (os-list (map (lambda (os)
                      (deploy-installer-os builder package os))
-                   (installer-data-os-list data))))))
+                   (installer-metadata-os-list data))))))
 
 (define (deploy-log-file builder package)
-  (let* ((source (website-package-log-file package))
-         (extension (file-extension source))
-         (log-file (string-replace-substring
-                    (website-package-name package)
-                    ".zip" (string-append ".log." extension)))
-         (target (string-append
-                  (installer-os-target-dir builder package)
-                  "/" log-file)))
-    (mkdir-p (dirname target))
-    (symlink source target)
-    (string-append (installer-os-relative-dir package) "/" log-file)))
+
+  (if (website-package-log-file package)
+      (let* ((source (website-package-log-file package))
+             (extension (file-extension source))
+             (log-file (string-replace-substring
+                        (website-package-name package)
+                        ".zip" (string-append ".log." extension)))
+             (target (string-append
+                      (installer-os-target-dir builder package)
+                      "/" log-file)))
+        (mkdir-p (dirname target))
+        (symlink source target)
+        (string-append (installer-os-relative-dir package) "/" log-file))
+      (format #t "Warning: No log file found for ~a.\n"
+              (derivation-name (website-package-derivation package)))))
 
 (define (deploy-package builder package)
   (website-package
    (inherit package)
-   (installer-data (deploy-package-installer-metadata builder package))
+   (installer-metadata (deploy-package-installer-metadata builder package))
    (log-file (deploy-log-file builder package))))
 
 (define (deploy-website-installer-metadata builder)
   (let ((target (website-builder-metadata-path builder))
-        (data (website-builder-installer-data builder)))
+        (data (website-builder-installer-metadata builder)))
     (mkdir-p (dirname target))
-    (write-installer-data data target)
+    (write-installer-metadata data target)
     builder))
 
 (define (deploy-packages builder)
@@ -227,8 +240,8 @@ a:active {
   (string-replace-substring (installer-os-package os) ".zip" ".json"))
 
 (define (website-sxml-installer-list-item package)
-  (let* ((data (website-package-installer-data package))
-         (os (car (installer-data-os-list data))))
+  (let* ((data (website-package-installer-metadata package))
+         (os (car (installer-metadata-os-list data))))
     `(li ,(installer-os-name os)
          " - "
          (a (@ (href ,(installer-os-package os)))
@@ -274,7 +287,8 @@ a:active {
     (let ((builder (website-builder
                     (inherit builder)
                     (packages (find-packages builder %store)))))
-      (render-website (deploy-website builder)))))
+      (when (website-builder-packages builder)
+        (render-website (deploy-website builder))))))
 
 ;; Getopt
 
@@ -302,7 +316,7 @@ a:active {
   (format #t  "  -o, --output-dir=DIR       Output directory (default: ~a)\n" %output-dir)
   (format #t  "  -s, --store-path=PATH      The path to the Guix store (default: ~a)\n" (%store-directory)))
 
-(define* (asahi-guix-nightly-main args)
+(define* (asahi-website-builder-main args)
   (let ((options (getopt-long args option-spec)))
     (if (option-ref options 'help #f)
         (show-usage)
@@ -312,6 +326,11 @@ a:active {
           (output-dir (output-dir-option options))
           (store-path (store-path-option options)))))))
 
-(define my-builder (asahi-guix-nightly-main '("nightly")))
+;; (define my-builder (asahi-website-builder-main '("nightly")))
+;; ;; (use-modules (ice-9 pretty-print))
+;; ;; (pretty-print m)
 
-;; (define my-paths (find-installer-package-derivation-paths "/gnu/store"))
+;; (define my-drvs
+;;   (filter derivation-output-path-exists? (find-installer-package-derivations "/gnu/store")))
+
+;; (pretty-print my-drvs)
