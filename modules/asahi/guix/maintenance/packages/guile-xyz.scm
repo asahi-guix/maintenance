061be32ca23e0c11a86be08b056fa3344d8b0d8f
Roman Scherer <roman@burningswell.com>
2025-02-09 13:04:55 +0100

    Add asahi-guix package to guix.scm

--- modules/asahi/guix/maintenance/packages/guile-xyz.scm
+++ modules/asahi/guix/maintenance/packages/guile-xyz.scm
@@ -1,10 +1,12 @@
 (define-module (asahi guix maintenance packages guile-xyz)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages gnupg)
   #:use-module (gnu packages guile)
   #:use-module (gnu packages package-management)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages)
   #:use-module (guix build-system gnu)
+  #:use-module (guix channels)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
@@ -27,6 +29,26 @@
               #:recursive? #t
               #:select? vcs-file?))
 
+(define asahi-guix
+  (guix-for-channels
+   (list (channel
+          (name 'guix)
+          (url "https://git.savannah.gnu.org/git/guix.git")
+          (introduction
+           (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+             "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+         (channel
+          (name 'asahi)
+          (url "https://github.com/asahi-guix/channel")
+          (branch "main")
+          (introduction
+           (make-channel-introduction
+            "3eeb493b037bea44f225c4314c5556aa25aff36c"
+            (openpgp-fingerprint
+             "D226 A339 D8DF 4481 5DDE  0CA0 3DDA 5252 7D2A C199")))))))
+
 (define-public asahi-guix-maintenance
   (package
     (name "asahi-guix-maintenance")
@@ -37,9 +59,8 @@
      ;; Use 'specification->package' to get the latest version of
      ;; the development packages.
      (append (map specification->package development-packages)
-             (list pkg-config guile-next guix)))
-    (inputs (list guile-next guix))
-    ;; (propagated-inputs (list guile-config guile-lib))
+             (list asahi-guix guile-git guile-gcrypt guile-sqlite3 pkg-config)))
+    (inputs (list guile-next guile-json-4))
     (synopsis "Asahi Guix Maintenance")
     (description "The docs, notes and code to maintain Asahi Guix.")
     (home-page "https://github.com/asahi-guix/maintenance")
